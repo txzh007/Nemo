b97c5dac1d4df38a703cb2ca502b8b1a0fcaacc0
tanxin <tanxin@zeaho.com>
2022-01-17 16:11:02 +0800

    init

---
+++ eureka/src/test/java/link/tanxin/nemo/eureka/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package link.tanxin.nemo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
